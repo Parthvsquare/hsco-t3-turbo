1729cc5336c2203df7ec56034650a655bb21030c
Parth V <26404957+Parthvsquare@users.noreply.github.com>
2024-11-29 04:19:54 +0530

    feat: using neon drizzle

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,7 +1,9 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
 import * as schema from "./schema";
+
+const sql = neon(process.env.POSTGRES_URL!);
 
 export const db = drizzle({
   client: sql,
