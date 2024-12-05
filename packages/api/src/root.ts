3580b6263fd702aee661dd4e74810ea8a3754dc3
SumanS <sumansasmal028@gmail.com>
2024-12-06 01:38:57 +0530

    added to root routes

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,14 @@
 import { authRouter } from "./router/auth";
+import { literRouter } from "./router/liter";
 import { postRouter } from "./router/post";
+import { weightRouter } from "./router/weight";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   post: postRouter,
+  weight: weightRouter,
+  liter: literRouter
 });
 
 // export type definition of API
